d98e98d5ba24b07b6c30b44db238fc9d355929de
Kyle Chapman <xeuoruxgmail.com>
2017-10-28 14:47:34 -0700

    Main Menu with Professional Crawl text

---
+++ Assets/CrawlTextMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrawlTextMover : MonoBehaviour
+{
+	public float speed;
+	public string mainScene;
+	public int waitingTime;
+
+	void Update ()
+	{
+		transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+		StartCoroutine(MoveToGameAfterTime());
+	}
+
+	private IEnumerator MoveToGameAfterTime()
+	{
+		yield return new WaitForSeconds(waitingTime);
+		MoveToGame();
+	}
+
+	public void MoveToGame()
+	{
+		SceneManager.LoadScene(mainScene);
+	}
+}
